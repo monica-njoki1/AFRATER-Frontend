d1b053b528491937a81fca9e2d1fb997d2be85e6
monica <monica.njoki@student.moringaschool.com>
2026-04-27 15:43:46 +0300

    button

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -80,8 +80,8 @@ export default function AuthModal({ isOpen, onClose, onAuthSuccess }) {
       >
         {/* Close button */}
         <button
-          onClick={handleClose}
           className={`fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50 p-4 ${!isOpen ? "hidden" : ""}`}
+          onClick={handleClose}
         >
           <X className="w-6 h-6" />
         </button>
